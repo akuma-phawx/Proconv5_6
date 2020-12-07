0e9154a1b8f3fa0ed9e1b62428c7df4316ab84cc
akuma-phawx <chrisvradis00@gmail.com>
2020-12-07 20:07:32 +0200

    keepoo

--- server.js
+++ server.js
@@ -52,9 +52,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/auth", authRoutes);
 app.use("/api", rateRoutes);
 
-app.use(express.static(path.join(dirname, "./dist")));
+app.use(express.static(path.join(__dirname, "./dist")));
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(dirname, "./dist", "index.html"));
+  res.sendFile(path.join(__dirname, "./dist", "index.html"));
 });
 //Handling Requests
 app.listen(PORT, () => {
