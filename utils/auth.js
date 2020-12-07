1fb4dac46a6b4d1f8172eff6eaffae40f762c0c6
akuma-phawx <chrisvradis00@gmail.com>
2020-12-07 21:46:24 +0200

    try catch

--- utils/auth.js
+++ utils/auth.js
@@ -1,13 +1,15 @@
 const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
-  const token = req.headers.authorization.replace("Bearer ", "");
-  console.log(token);
-  const decoded = jwt.verify(token, "hiremepliz");
-  req.userData = decoded;
-  // console.log(req.userData);
-  next();
-
-  return res.status(401).json({
-    message: "Authentification Failed",
-  });
+  try {
+    const token = req.headers.authorization.replace("Bearer ", "");
+    console.log(token);
+    const decoded = jwt.verify(token, "hiremepliz");
+    req.userData = decoded;
+    // console.log(req.userData);
+    next();
+  } catch (e) {
+    return res.status(401).json({
+      message: "Authentification Failed",
+    });
+  }
 };
